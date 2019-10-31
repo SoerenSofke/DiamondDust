af52b8e61728a1a43b42fef6e7ac28e62c4b2469
soerensofke <soeren.sofke@gmail.com>
2019-10-31 21:36:48 +0000

    style adjusted with iStyle

--- soc/syscon/syscon.v
+++ soc/syscon/syscon.v
@@ -1,49 +1,51 @@
 `timescale 1ns/100ps
 
 module syscon (
-    output reg clk,
-    output reg rst = 1
-);
-    // Internal states
-    reg enable = 0;
-    reg next_enable;
-    reg next_reset;
+           output reg clk,
+           output reg rst = 1
+       );
+// Internal states
+reg enable = 0;
+reg next_enable;
+reg next_reset;
 
-    // Combinational process
-    always @(*) begin
-        if (enable == 0) begin
-            next_reset = 1;
-            next_enable = 1;
-        end else begin
-            next_reset = 0;
-        end
+// Combinational process
+always @( * ) begin
+    if ( enable == 0 ) begin
+        next_reset = 1;
+        next_enable = 1;
     end
-
-    // Sequential process
-    always @(posedge clk) begin
-        enable <= next_enable;
-        rst <= next_reset;
+    else begin
+        next_reset = 0;
     end
+end
 
-    // Clock generation
-    `ifdef SIMULATION // Simulation only
-        initial begin
-            clk = 0;
-            $dumpfile("build/top.vcd");
-            $dumpvars;
-            #`SIMULATION $finish;
-        end
+// Sequential process
+always @( posedge clk ) begin
+    enable <= next_enable;
+    rst <= next_reset;
+end
 
-        always begin
-            #0.5 clk = !clk;
-        end
-    `else // Synthesis only
-        SB_HFOSC #(
-            .CLKHF_DIV ("0b11") // https://www.latticesemi.com/-/media/LatticeSemi/Documents/ApplicationNotes/IK/iCE40OscillatorUsageGuide.ashx?document_id=50670
-        ) OSC_inst0 (
-            .CLKHFEN(1'b1),
-            .CLKHFPU(1'b1),
-            .CLKHF(clk)
-        );
-    `endif
+// Clock generation
+`ifdef SIMULATION // Simulation only
+initial begin
+    clk = 0;
+    $dumpfile( "build/top.vcd" );
+    $dumpvars;
+    #`SIMULATION $finish;
+end
+
+always begin
+    #0.5 clk = !clk;
+end
+`else // Synthesis only
+SB_HFOSC #(
+             // https://www.latticesemi.com/-/media/LatticeSemi/Documents/ApplicationNotes/IK/iCE40OscillatorUsageGuide.ashx
+             .CLKHF_DIV ( "0b11" )
+         ) OSC_inst0 (
+             .CLKHFEN( 1'b1 ),
+             .CLKHFPU( 1'b1 ),
+             .CLKHF( clk )
+         );
+`endif
 endmodule
--- soc/syscon/syscon_top.v
+++ soc/syscon/syscon_top.v
@@ -1,48 +1,54 @@
 `timescale 1ns/1ns
 
 module syscon_top (
-    output reg clk_pin = 0,
-    output reg rst_pin = 1
-);
-    wire clk;
-    wire rst;
+           output reg clk_pin = 0,
+           output reg rst_pin = 1
+       );
+wire clk;
+wire rst;
 
-    always @(posedge clk) begin
-        clk_pin <= !clk_pin;
-        rst_pin <= rst;
+always @( posedge clk ) begin
+    clk_pin <= !clk_pin;
+    rst_pin <= rst;
+end
+
+syscon syscon_inst0 (
+           .clk( clk ),
+           .rst( rst )
+       );
+
+// Unit test
+`ifdef SIMULATION
+
+testAnythingProtocol #( "build", "report.tap" ) tap ();
+reg[ 63 * 8: 0 ] test_message;
+integer cycleCounter = 0;
+
+always @( posedge clk ) begin
+    cycleCounter = cycleCounter + 1;
+
+    // reset pin level: 1 1 1 0 0 0 0 0...
+    $sformat( test_message, "Reset Pin Level: %d", rst_pin );
+    if ( cycleCounter <= 3 ) begin
+        tap.assert( rst_pin == 1, test_message );
+    end
+    else begin
+        tap.assert( rst_pin == 0, test_message );
     end
 
-    syscon syscon_inst0 (
-        .clk(clk),
-        .rst(rst)
-    );
+    // clock pin level: 1 0 1 0 1 0 1 0...
+    $sformat( test_message, "Clock Pin Level: %d", clk_pin );
+    if ( cycleCounter % 2 == 0 ) begin
+        tap.assert( clk_pin == 1, test_message );
+    end
+    else begin
+        tap.assert( clk_pin == 0, test_message );
+    end
 
-    // Unit test
-    `ifdef SIMULATION
-        testAnythingProtocol #("build", "report.tap") tap ();
-        reg[63*8:0] test_message;
-        integer cycleCounter = 0;
-
-        always @(posedge clk) begin
-            cycleCounter = cycleCounter + 1;
-
-            // reset pin level: 1 1 1 0 0 0 0 0...
-            $sformat(test_message, "Reset Pin Level: %d", rst_pin);
-            if (cycleCounter <= 3)
-                tap.assert(rst_pin == 1, test_message);
-            else
-                tap.assert(rst_pin == 0, test_message);
-
-            // clock pin level: 1 0 1 0 1 0 1 0...
-            $sformat(test_message, "Clock Pin Level: %d", clk_pin);
-            if (cycleCounter % 2 == 0)
-                tap.assert(clk_pin == 1, test_message);
-            else
-                tap.assert(clk_pin == 0, test_message);
-
-            // finish after 10 cycles
-            if (cycleCounter >= 10)
-                tap.finish;
-        end
-    `endif
+    // finish after 10 cycles
+    if ( cycleCounter >= 10 ) begin
+        tap.finish;
+    end
+end
+`endif
 endmodule
