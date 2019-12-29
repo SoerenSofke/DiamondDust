f14291ec4cc2b2a4c29fe3012953abb1968ce5da
soerensofke <soeren.sofke@gmail.com>
2019-12-29 16:46:00 +0000

    only one process any more

--- soc/rom/rom_wb.v
+++ soc/rom/rom_wb.v
@@ -13,33 +13,27 @@ module rom_wb #(
            /* verilator lint_on UNUSED */
        );
 
-// Initialization
+// Internal States
 reg [ 31: 0 ] rom [ 0: 3071 ];
+reg [ 11: 0 ] address;
+
+// Initialization
 initial begin
     $readmemh( ROM_IMAGE, rom );
 end
 
-// Combinational process
-reg [ 11: 0 ] address;
-reg [ 31: 0 ] next_data;
-reg next_ack;
-always @( * ) begin
+// Cloed process with algoritm
+always @( posedge clk ) begin
     if ( wbs_cyc_i == 1 && wbs_stb_i == 1 && ~wbs_ack_o ) begin
         address = wbs_adr_i[ 13: 2 ];
-        next_data = rom[ address ];
-        next_ack = 1;
+        wbs_dat_o = rom[ address ];
+        wbs_ack_o = 1;
     end
     else begin
         address = 12'hXXX;
-        next_data = 32'hXXXXXXXX;
-        next_ack = 0;
+        wbs_dat_o = 32'hXXXXXXXX;
+        wbs_ack_o = 0;
     end
-end
-
-// Sequential process
-always @( posedge clk ) begin
-    wbs_dat_o <= next_data;
-    wbs_ack_o <= next_ack;
 end
 
 endmodule
